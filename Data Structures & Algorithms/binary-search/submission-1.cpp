188f02c6b3a061852699e28fffbb3dd6c4396b47
NITHIN N <nithin.engineer.ai@gmail.com>
2026-08-26 11:28:59 +0530

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+
+
+        int i = 0,j = nums.size();
+
+        while(i<j){
+            int mid = i+(j-i)/2;
+            if(nums[mid] == target)return mid;
+            else if(nums[mid] < target){
+                i = mid+1;
+            }else{
+                j = mid ;
+            }
+        }
+
+        return -1;
+        
+    }
+};
