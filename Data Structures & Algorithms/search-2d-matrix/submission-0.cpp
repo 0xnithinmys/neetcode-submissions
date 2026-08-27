8c0ec6fc7145e7b4e65597da09a1b02f895ed034
NITHIN N <nithin.engineer.ai@gmail.com>
2026-08-27 23:58:07 +0530

    Add: search-2d-matrix - submission-0

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+
+class Solution {
+public:
+    bool searchMatrix(std::vector<std::vector<int>>& matrix, int target) {
+        if (matrix.empty() || matrix[0].empty()) return false;
+        
+        int m = matrix.size();      
+        int n = matrix[0].size();   
+        
+        int low = 0;
+        int high = (m * n) - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            int row = mid / n;
+            int col = mid % n;
+            
+            int mid_element = matrix[row][col];
+            
+            if (mid_element == target) {
+                return true;
+            } else if (mid_element < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        
+        return false;
+    }
+};
