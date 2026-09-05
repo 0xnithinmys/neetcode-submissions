47bf0b7f26157c264c280572aeb9487bb0e6df12
NITHIN N <nithin.engineer.ai@gmail.com>
2026-09-06 00:03:02 +0530

    Add: linked-list-cycle-detection - submission-0

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-0.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+
+        unordered_map<ListNode*,int> u;
+
+
+       ListNode* t = head;
+
+
+       while(t){
+        u[t]++;
+        if(u[t] >1){
+            return true;
+        }
+
+        t = t->next;
+       } 
+
+
+       return false;
+        
+    }
+};
